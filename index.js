847732038622dcc921787eb2ece28a424f43e310
adi dabush <adi.dabush@wework.com>
2019-07-19 01:26:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() == string )
-  return "I can't hear you!";
-  return "YES INDEED!";
-  return "I love you, too."
+  if (string===string.toLowerCase()) { 
+    return 'I can\'t hear you!'
+  } else if (string===string.toUpperCase()) {
+    return 'YES INDEED!'   
+   } else (string==="I love you, Grandma."); {
+    return "I love you, too."
+  }
 }
